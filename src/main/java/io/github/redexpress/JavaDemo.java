2ff3de419dc8d5136cf13e872b6acec88c3a4af7
Gavin Yang <redppl@hotmail.com>
2018-10-07 11:26:07 +0800

    add mven, ant

---
+++ src/main/java/io/github/redexpress/JavaDemo.java
@@ -0,0 +1,7 @@
+package io.github.redexpress;
+
+public class JavaDemo {
+    public static void main( String[] args ) {
+        System.out.println("JavaDemo!" );
+    }
+}
---
+++ src/test/java/io/github/redexpress/JavaDemoTest.java
@@ -0,0 +1,11 @@
+package io.github.redexpress;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class JavaDemoTest { 
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
